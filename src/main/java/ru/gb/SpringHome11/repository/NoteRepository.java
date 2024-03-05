4477275e1787ae37a7052fa722281df3e3c410dc
Anastasia Elfimova <nastya.elfimova@inbox.ru>
2024-03-05 09:41:06 +0500

    HW11 id rady

---
+++ src/main/java/ru/gb/SpringHome11/repository/NoteRepository.java
@@ -0,0 +1,9 @@
+package ru.gb.SpringHome11.repository;
+
+import ru.gb.SpringHome11.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
---
+++ src/main/java/ru/gb/SpringHome11Application.java
@@ -0,0 +1,11 @@
+package ru.gb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHome11Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHome11Application.class, args);
+    }
+}
